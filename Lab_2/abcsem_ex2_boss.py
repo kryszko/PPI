6d7f6a59b2dcb656b412fb92ee92803f79fc42d6
kryszko <krypieli@gmail.com>
2024-11-19 17:37:50 +0100

    zadanie 1 i 2 bosem zrobione

--- Lab_2/abcsem_ex2_boss.py
+++ Lab_2/abcsem_ex2_boss.py
@@ -8,34 +8,36 @@ semC = Semaphore(0)
 
 def printA(ntimes):
     for i in range(ntimes):
-        semB.release()          #zapytanie b
-        semA.acquire()          #odpowiedz b
-        semC.release()          #zapytanie c
-        semA.acquire()          #odpowiedz c
-        semB.release()          #zapytanie b2
-        semA.acquire()          #odowiedz b2
+        semA.acquire()
         print('A ', end="")
         time.sleep(1)
+        semB.release()
 
 def printB(ntimes):
     for i in range(ntimes):
+        semA.release()
+        semB.acquire()
+        semC.release()
         semB.acquire()
         print('B ', end="")
         time.sleep(1)
         semA.release()
+        semB.acquire()
+        semC.release()
+        semB.acquire()
 
 def printC(ntimes):
     for i in range(ntimes):
         semC.acquire()
         print('C ', end="")
         time.sleep(1)
-        semA.release()
+        semB.release()
 
 how_many = 2
 threads = []
-threads.append(Thread(target=printA, args=(how_many,)))
-threads.append(Thread(target=printB, args=(how_many*2,)))
-threads.append(Thread(target=printC, args=(how_many,)))
+threads.append(Thread(target=printA, args=(how_many*2,)))
+threads.append(Thread(target=printB, args=(how_many,)))
+threads.append(Thread(target=printC, args=(how_many*2,)))
 
 for thread in threads:
     thread.start()
